2347b76b20acf0f807b835a61489daf651691f4a
George Lambeth <glambeth1594@gmail.com>
2017-02-03 13:40:55 -0500

    1.6.0 fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /home/george/optiminer-zcash-1.5.0.tar.gz https://github.com/Optiminer/OptiminerZcash/blob/master/optiminer-zcash-1.6.0.tar.gz
+wget -O /home/george/optiminer-zcash-1.6.0.tar.gz https://github.com/Optiminer/OptiminerZcash/blob/master/optiminer-zcash-1.6.0.tar.gz
 tar -xvf /home/george/optiminer-zcash-1.6.0.tar.gz -C /home/george
 echo "#!/bin/bash
 export GPU_FORCE_64BIT_PTR=1
